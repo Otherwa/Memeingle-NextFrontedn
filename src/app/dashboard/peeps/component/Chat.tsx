bd151f6fb4a3252219ba17285fe6dfc8652c7d48
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2024-07-14 20:20:29 +0530

    /fxies/

--- src/app/dashboard/peeps/component/Chat.tsx
+++ src/app/dashboard/peeps/component/Chat.tsx
@@ -92,7 +92,6 @@ const Chat: React.FC<ChatProps> = ({ userId }) => {
                         <Skeleton className="h-[125px] w-[250px] rounded-xl" />
                         <div className="space-y-2">
                             <Skeleton className="h-4 w-[250px]" />
-                            <Skeleton className="h-4 w-[200px]" />
                         </div>
                     </div>
                 </div>
