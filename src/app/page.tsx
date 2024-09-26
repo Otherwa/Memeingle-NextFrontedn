24658491dd927936bbafb8539659c98e77ea8ef8
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2024-09-26 22:26:14 +0530

    //

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,6 @@ export default function Home() {
                 <AspectRatio ratio={16 / 9} className="bg-muted">
                   <Image
                     src="https://cdn.myportfolio.com/e1ad70d8-3a17-4e90-9c11-81c3e6e24cbf/641430ca-942d-4446-9859-6fb893cab2a4_rw_1200.gif?h=40fa44581f5a6a4ae191d5305a2131f9"
-                    alt="Photo by Drew Beamer"
                     className="rounded-md object-cover"
                   />
                 </AspectRatio>
