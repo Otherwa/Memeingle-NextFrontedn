091376ee0daace9d277cfcddc812adeabbff4026
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2024-11-05 16:39:18 +0530

    /minor fixes/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,12 @@ export const metadata = {
   description: "Connect with peeps",
   openGraph: {
     type: 'website',
-    url: 'https://memeingle-next-frontedn.vercel.app/',
+    url: 'https://memeingle.vercel.app/',
     title: 'Memeingle',
     description: 'Connect with peeps',
     images: [
       {
-        url: 'https://memeingle-next-frontedn.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fkftzwdyauwt9%2F3cSZykppt940Q5vMHjbYd8%2Ff111f0645166996fd77bd543d4cfd238%2Fspring_updates.jpg%3Fw%3D828%26q%3D90%26fm%3Dwebp&w=1920&q=75',
+        url: 'https://memeingle.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fkftzwdyauwt9%2F3cSZykppt940Q5vMHjbYd8%2Ff111f0645166996fd77bd543d4cfd238%2Fspring_updates.jpg%3Fw%3D828%26q%3D90%26fm%3Dwebp&w=1920&q=75',
         width: 800,
         height: 600,
         alt: 'Memeingle'
@@ -28,7 +28,7 @@ export const metadata = {
     description: 'Connect with peeps',
     images: [
       {
-        url: 'https://memeingle-next-frontedn.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fkftzwdyauwt9%2F3cSZykppt940Q5vMHjbYd8%2Ff111f0645166996fd77bd543d4cfd238%2Fspring_updates.jpg%3Fw%3D828%26q%3D90%26fm%3Dwebp&w=1920&q=75',
+        url: 'https://memeingle.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fkftzwdyauwt9%2F3cSZykppt940Q5vMHjbYd8%2Ff111f0645166996fd77bd543d4cfd238%2Fspring_updates.jpg%3Fw%3D828%26q%3D90%26fm%3Dwebp&w=1920&q=75',
         width: 800,
         height: 600,
         alt: 'Memeingle'
